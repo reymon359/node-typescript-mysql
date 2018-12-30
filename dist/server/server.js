5f06e2bd6897743eacc7989ac8b0572865cf0085
ramon morcillo <ramonmorcillocascales@gmail.com>
2018-12-30 12:14:16 +0100

    deploying the public folder

--- dist/server/server.js
+++ dist/server/server.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
+const path = require("path");
 // Export is to export the class to other files and 
 // default is to say that this will be the class exported by default
 class Server {
@@ -12,8 +13,14 @@ class Server {
     static init(port) {
         return new Server(port);
     }
+    // To show the public folder
+    publicFolder() {
+        const publicPath = path.resolve(__dirname, '../public');
+        this.app.use(express.static(publicPath));
+    }
     start(callback) {
         this.app.listen(this.port, callback);
+        this.publicFolder();
     }
 }
 exports.default = Server;
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,5 +1,5 @@
 import express = require('express');
-
+import path = require('path');
 
 // Export is to export the class to other files and 
 // default is to say that this will be the class exported by default
@@ -18,7 +18,16 @@ export default class Server{
         return new Server(port);
     }
 
+    // To show the public folder
+    private publicFolder(){
+        const publicPath = path.resolve(__dirname, '../public');
+
+        this.app.use(express.static(publicPath));
+    }
+    
+
     start(callback:Function){
         this.app.listen(this.port, callback);
+        this.publicFolder();
     }
 }
